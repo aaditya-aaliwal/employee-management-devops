6d37b47c6fb49ca71a801187f8528adc92f2a8ec
Aaditya Aaliwal <aadityaaaliwal@gmail.com>
2026-06-16 11:29:28 +0530

    Complete Phase 1 CRUD application

--- EmployeeManagementSystem/Controllers/EmployeesController.cs
+++ EmployeeManagementSystem/Controllers/EmployeesController.cs
@@ -9,9 +9,17 @@ namespace EmployeeManagementSystem.Controllers
     {
         private EmployeeDbContext db = new EmployeeDbContext();
 
-        public ActionResult Index()
+        public ActionResult Index(string searchTerm)
         {
-            return View(db.Employees.ToList());
+            var employees = db.Employees.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                employees = employees.Where(e =>
+                    e.Name.Contains(searchTerm));
+            }
+
+            return View(employees.ToList());
         }
 
         public ActionResult Create()
@@ -30,6 +38,62 @@ namespace EmployeeManagementSystem.Controllers
             }
 
             return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var employee = db.Employees.Find(id);
+
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State =
+                    System.Data.Entity.EntityState.Modified;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var employee = db.Employees.Find(id);
+
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var employee = db.Employees.Find(id);
+
+            db.Employees.Remove(employee);
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }

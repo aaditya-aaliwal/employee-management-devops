1ec68faf886d2eee90dd58e96c2ec0c08d0c753c
Aaditya Aaliwal <aadityaaaliwal@gmail.com>
2026-06-17 18:30:23 +0530

    fix: set Employees as default route

--- EmployeeManagementSystem/App_Start/RouteConfig.cs
+++ EmployeeManagementSystem/App_Start/RouteConfig.cs
@@ -16,7 +16,12 @@ namespace EmployeeManagementSystem
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new
+                {
+                    controller = "Employees",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
             );
         }
     }
